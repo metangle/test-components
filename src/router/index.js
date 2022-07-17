20e6d587300226dff3e21c8e5e72228918a2484f
zhuronggui <zrg_dev@163.com>
2022-07-17 16:10:56 +0800

    fix: 去掉demo路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 const Home = () => import('../views/Home.vue')
-const Demo = () => import('../views/Demo.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
   {
     path: '/',
-    name: 'Demo',
-    component: Demo
-  },
+    name: 'Home',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
